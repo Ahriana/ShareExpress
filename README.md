ed83172788c358a2743e0abdde48d7d73b589c6b
rosiecode <rosiecode@outlook.com>
2018-01-03 04:59:23 +0000

    working checkpoint

---
+++ README.md
@@ -0,0 +1,12 @@
+# ShareX example config
+```
+{
+  "Name": "RC test",
+  "DestinationType": "ImageUploader",
+  "RequestURL": "http://localhost:3030/upload",
+  "FileFormName": "file",
+  "Arguments": {
+    "name": "%h.%mi.%s-%d.%mo.%yy"
+  }
+}
+```
--- init.js
+++ init.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const path = require('path');
 const helmet = require('helmet');
 const http = require('http');
 const config = require('./config.json');
 const fs = require('fs');
 const util = require('util');
-const multipart = require('connect-multiparty');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, `${__dirname}/public/images`);
+    },
+    filename: (req, file, cb) => {
+        cb(null, `image-${Date.now()}.png`);
+    },
+});
+
+const upload = multer({ storage });
 
 function writeLog(log) {
-    if (typeof log === 'object') { log = util.inspect(log) }
+    if (typeof log === 'object') { log = util.inspect(log); }
     console.log('New Log:', log);
     fs.appendFile('error.log', `${log}\n`, (err) => {
         if (err) throw err;
@@ -20,10 +30,6 @@ function writeLog(log) {
 
 app.use(helmet());
 app.set('view engine', 'hbs');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multipart());
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -31,16 +37,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/view/:id', (req, res) => {
-    res.render('view', { name: config.general.serverName });
+    res.render('view', {
+        name: config.general.serverName,
+        image: req.params.id,
+        fullUrl: config.general.serverPort !== 80 ? `${config.general.serverLocation}:${config.general.serverPort}/view/${req.params.id}` : `${config.general.serverLocation}/view/${req.params.id}`,
+    });
 });
 
-app.post('/upload', (req, res) => {
-    //writeLog(`userkey: ${req.body.key}`);
-    //writeLog(`filename: ${req.body.name}`);
-    //writeLog(`filename: ${req.file}`);
-    // writeLog(req.files.file);
-
-    res.status(200).send('thonk');
+app.post('/upload', upload.single('file'), (req, res) => {
+    console.log('file Details:', req.file);
+    console.log('Req Details:', req.body);
+    if (req.body.key !== config.creds.key) { return res.status(401).send('nope'); }
+    res.send(config.general.serverPort !== 80 ? `${config.general.serverLocation}:${config.general.serverPort}/view/${req.file.filename}` : `${config.general.serverLocation}/view/${req.file.filename}`);
 });
 
 app.get('*', (req, res) => {
@@ -48,7 +56,7 @@ app.get('*', (req, res) => {
 });
 
 app.use((err, req, res, next) => {
-    writeLog(err)
+    writeLog(err);
     return res.status(500).sendFile(path.join(`${__dirname}/pages/500.html`));
 });
 
