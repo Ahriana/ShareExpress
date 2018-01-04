b815b6841ae0b6ab3917ee8c53feb843649bff4d
rosiecode <rosiecode@outlook.com>
2018-01-04 03:17:07 +0000

    user support

--- init.js
+++ init.js
@@ -8,7 +8,11 @@ const fs = require('fs');
 const util = require('util');
 const multer = require('multer');
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => { cb(null, `${__dirname}/public/images`); },
+    destination: (req, file, cb) => {
+        const dir = `${__dirname}/public/images/${req.body.userName}`;
+        if (!fs.existsSync(dir)) { fs.mkdirSync(dir); }
+        cb(null, dir);
+    },
     filename: (req, file, cb) => { cb(null, encodeURI(file.originalname)); },
 });
 const upload = multer({ storage });
@@ -31,23 +35,33 @@ function getURL() {
     }
 }
 
+function failed(req, res, err, code) {
+    writeLog(`${code}: ${err}`);
+    writeLog(req.body);
+    res.status(code).send(err);
+}
+
 config.base = getURL();
 app.use(helmet());
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => { res.render('index', { name: config.general.serverName }); });
 
-app.get('/view/:id', (req, res) => {
+app.get('/view/:user/:id', (req, res) => {
     res.render('view', {
         name: config.general.serverName,
-        image: req.params.id,
-        fullImage: `${config.base}/images/${req.params.id}`,
+        image: `${req.params.user}/${req.params.id}`,
+        user: req.params.user,
+        fullImage: `${config.base}/images/${req.params.user}/${req.params.id}`,
     });
 });
 
 app.post('/upload', upload.single('file'), (req, res) => {
-    if (req.body.key !== config.creds.key) { return res.status(401).send('nope'); }
-    res.send(`${config.base}/view/${req.file.filename}`);
+
+    if (!req.body.userName) { return failed(req, res, 'invalid user', 401); }
+    if (req.body.key !== config.users[req.body.userName].key) { return failed(req, res, 'invalid token', 401); }
+
+    res.send(`${config.base}/view/${req.body.userName}/${req.file.filename}`);
 });
 
 app.get('*', (req, res) => { res.status(404).sendFile(path.join(`${__dirname}/pages/404.html`)); });
